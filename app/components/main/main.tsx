bbdebb728095e9608f27c917da25e2ff4b433967
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-11 18:59:52 -0300

    feat: create main component

---
+++ app/components/main/main.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Sobre from "./about/about";
+import Skills from "./skills/skills";
+
+function Main() {
+    return (
+        <main>
+            <Sobre />
+            <Skills />
+        </main>
+    );
+}
+
+export default Main;
