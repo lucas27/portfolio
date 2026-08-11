fe157d07df3ec60b5cbf106605d63945d46b6054
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-11 18:58:33 -0300

    feat: add main components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import Image from "next/image";
-import { Header } from "./components/header";
+import { Header } from "./components/header/header";
+import Main from "./components/main/main";
 
 export default function Home() {
     return (
-        <Header />
+        <body>
+            <Header />
+            <Main />
+        </body>
     );
 }
