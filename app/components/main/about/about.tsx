6baf908d6fb981e6303c6cb46dc70db601dc557f
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-11 19:00:01 -0300

    feat: create about component

---
+++ app/components/main/about/about.tsx
@@ -0,0 +1,18 @@
+function about() {
+    return (
+        <div id="sobre" className="flex flex-col text-center p-10 gap-4">
+            <h1 className="text-2xl font-[Roboto]">Sobre Mim</h1>
+            <div className="flex justify-center">
+                <p className="w-220 text-lg font-[Roboto]">
+                    Graduado em Engenharia da Computação, com pós-graduação em Análise de Sistemas e MBA em Inteligência Artificial.
+                    <br /> 
+                    Desenvolvedor com foco em Back-end utilizando Java e Spring Boot, experiência prática no desenvolvimento 
+                    de APIs REST, autenticação com JWT, modelagem de bancos de dados relacionais e aplicações Full Stack com React 
+                    e Angular.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default about;
