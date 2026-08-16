d81cafd727f6f3dfd1332f026d3c5cfe2d28541d
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-16 17:17:42 -0300

    feat: add context

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 import Image from "next/image";
 import { Header } from "./components/header/header";
 import Main from "./components/main/main";
+import ThemeProvider from "./utils/context";
 
 export default function Home() {
     return (
         <body>
-            <Header />
-            <Main />
+            <ThemeProvider>
+                <Header />
+                <Main />
+            </ThemeProvider>
         </body>
     );
 }
---
+++ app/utils/context.tsx
@@ -0,0 +1,19 @@
+import { createContext,  Dispatch,  ReactNode,  SetStateAction,  useState  } from "react";
+
+export interface contextType {
+    themeMode: string,
+    setThemeMode: Dispatch<SetStateAction<string>>
+};
+
+export const ThemeContext = createContext<contextType | undefined >(undefined);
+
+const ThemeProvider = ({children}: {children: ReactNode}) => {
+    const [themeMode, setThemeMode] = useState<string>("black");
+    return (
+        <ThemeContext.Provider value={{themeMode, setThemeMode}}>
+          {children}  
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider;
