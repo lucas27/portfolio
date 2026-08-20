6d498893e4d534c04bb886a78b674f6de16a86a5
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-19 21:12:31 -0300

    feat: add projects

--- app/components/main/main.tsx
+++ app/components/main/main.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Sobre from "./about/about";
+import Projects from "./projects/projects";
 import Skills from "./skills/skills";
 
 function Main() {
     return (
-        <main>
+        <main className="flex flex-col gap-20">
             <Sobre />
             <Skills />
+            <Projects />
         </main>
     );
 }
