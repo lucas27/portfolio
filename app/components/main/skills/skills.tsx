be0a1d088a4edacd1f7c716d739dfd1c14f6995c
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-23 20:49:12 -0300

    feat: change skills structure

--- app/components/main/skills/skills.tsx
+++ app/components/main/skills/skills.tsx
@@ -1,19 +1,74 @@
-'use client'
-import style from './skills.module.css'
+import java from '@/public/icons8-java.svg';
+import spring from '@/public/icons8-spring-boot.svg';
+import typescript from '@/public/icons8-typescript.svg';
+import javascript from '@/public/icons8-javascript.svg';
+import react from '@/public/icons8-react-24.png';
+import angular from '@/public/icons8-angularjs.svg';
+import next from '@/public/icons8-nextjs.svg';
+import tailwind from '@/public/icons8-tailwind-css.svg';
+import sass from '@/public/icons8-sass-24.png';
+import git from '@/public/icons8-git.svg';
+import postgres from '@/public/icons8-postgresql.svg';
+import rabbit from '@/public/rabbitmq-logo-svgrepo-com.svg';
+import docker from '@/public/icons8-docker-24.png';
+
+import { Suspense } from 'react';
 function Skills() {
+    const BoxTechnologie = () => {
+        const technologieIcons = [
+            java.src,
+            spring.src,
+            javascript.src,
+            typescript.src,
+            angular.src,
+            react.src,
+            next.src,
+            tailwind.src,
+            sass.src,
+            postgres.src,
+            git.src,
+            rabbit.src,
+            docker.src
+        ];
+        const textIcons = [
+            "Java",
+            "Spring Boot",
+            "Javascript",
+            "Typescript",
+            "Angular",
+            "React",
+            "Next.js",
+            "Tailwind CSS",
+            "Sass",
+            "PostgresSQL",
+            "Git",
+            "RabbitMQ",
+            "Docker"
+        ]
+
+        return technologieIcons.map((icon, index) => (
+            <li key={index} 
+            className="flex w-60 h-12 justify-center items-center gap-2 font-[Roboto] text-lg font-[600] border-2 border-[gray] rounded-xl"
+            >
+                <img src={icon} 
+                className="size-8"
+                />
+                {textIcons[index]}
+            </li>
+        ));
+    };
+
     return (
-        <nav className="flex flex-col items-center text-center gap-5">
-            <h2 className="text-2xl font-[Roboto]">Habilidades & Tecnologias</h2>
-            <fieldset className="flex gap-3 flex-wrap w-220">
-                <a className={style.skillLink} href="https://www.w3schools.com/java/java_getstarted.asp">Java</a>
-                <a className={style.skillLink} href="https://spring.io">Spring Boot</a>
-                <a className={style.skillLink} href="https://www.typescriptlang.org">Typescript</a>
-                <a className={style.skillLink} href="https://angular.dev/overview">AngularJs</a>
-                <a className={style.skillLink} href="https://react.dev/learn">ReactJs</a>
-                <a className={style.skillLink} href="https://nextjs.org/docs/app/getting-started">NextJs</a>
-                <a className={style.skillLink} href="https://tailwindcss.com">Tailwindcss</a>
-            </fieldset>
-        </nav>
+        <section className="flex flex-col gap-10 pb-10 pl-105 relative max-w-500 select-none">
+            <h2 id="habilidades" 
+            className="flex text-4xl font-[Roboto] after:border-b-2 after:absolute after:w-22 after:h-11 after:border-red-500"
+            >Habilidades & Tecnologias</h2>
+            <ul className="flex gap-5 flex-wrap">
+                <Suspense>
+                    <BoxTechnologie />
+                </Suspense>
+            </ul>
+        </section>
     );
 }
 
