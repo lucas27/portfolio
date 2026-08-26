5c0c4f302248d3f30ebead1d51f42667430e13f3
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-26 18:29:09 -0300

    feat: navigation change and style buttons

--- app/components/main/about/components/linkButtons.tsx
+++ app/components/main/about/components/linkButtons.tsx
@@ -7,10 +7,11 @@ import blackIconLinkedin from '@/public/icons8-linkedin-black.svg';
 import blackIconEmail from '@/public/mail_24dp_BLACK_FILL0_wght400_GRAD0_opsz24.svg';
 
 import { ThemeContext } from "@/app/utils/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import jsonFile from '../file/aboutExtraComponentText.json';
 
 export function LinksButton() {
+    const [isHover, setIsHover] = useState<number | null>(null);
     const color = useContext(ThemeContext);
     const textColor = color?.themeMode;
     const linksButton = jsonFile.links;
@@ -18,22 +19,40 @@ export function LinksButton() {
 
     const icon = [
         whiteIconOpenNew.src,
-        null,
+        textColor?.includes("black") ? blackIconEmail.src : whiteIconEmail.src,
         textColor?.includes("black") ? blackIconGithub.src : whiteIconGithub.src,
         textColor?.includes("black") ? blackIconLinkedin.src : whiteIconLinkedin.src,
         textColor?.includes("black") ? blackIconEmail.src : whiteIconEmail.src,
     ];
 
+    const handleScrollSection = (sectionId: string) => {
+        if(!sectionId.includes("#") ) {
+            window.open(sectionId, "_blank")
+        }else {
+            const section = document.querySelector(sectionId);
+            section?.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return linksButton.map((link, index) => (
         <a 
         key={index} 
-        href={link} 
+        onClick={() => handleScrollSection(link)}
+        onMouseEnter={() => setIsHover(index)}
+        onMouseLeave={() => setIsHover(null)} 
         className={`flex border-2 rounded-lg p-2 gap-1 select-none
-            ${(textColor?.includes("black") && index !== 0) ? "text-black shadow-sm" : "text-[#FAFAFA]"}
-            ${index !== 1 && index !== 0 ? "border-none bg-[#141c2c]" : null} 
-            ${(index !== 1 && index !== 0 && textColor?.includes("black")) ? "border-none shadow-lg bg-white" : null}
-            ${index === 0 ? `bg-[#FF3B3B] border-transparent text-[#FAFAFA]` : `border-[grey]`}`} 
-            download
+            ${(textColor?.includes("black") && index !== 0) ? " text-black shadow-sm" : "text-[#FAFAFA]"}
+            ${index !== 1 && index !== 0 ? " bg-[#141c2c]" : null} 
+            ${(index !== 1 && index !== 0 && textColor?.includes("black")) ? `shadow-[0_0_15px_rgba(0,0,0,0.3)]
+            // não é bonito, mas foi um jeito de manter a gambiarra 
+            ${(isHover !== 0 && isHover !== 1 && isHover === index) ?  "cursor-pointer border-red-500 shadow-red-500 shadow-lg bg-transparent" : "bg-transparent border-transparent "}
+            ` 
+            : (isHover !== 0  && isHover === index) ?  "border-red-500 shadow-[red] shadow-sm cursor-pointer" 
+            : index === 1 ? " border-[grey]" : "border-transparent"}
+            ${index === 0 ? `cursor-pointer bg-[#FF3B3B] border-transparent text-[#FAFAFA]` : `border-[grey]`}     
+            `}
+            
+            // download
         >
             <img 
             src={ icon[index] }
