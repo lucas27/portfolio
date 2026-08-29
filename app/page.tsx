04b6cbbb0c523e5adcf8cdf1d1244756fe7e6d4e
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-28 21:13:06 -0300

    feat: create footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Header } from "./components/header/header";
 import Main from "./components/main/main";
 import ThemeProvider from "./utils/context";
+import Footer from "./components/footer/footer";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <ThemeProvider>
                 <Header />
                 <Main />
+                <Footer />
             </ThemeProvider>
         </body>
     );
