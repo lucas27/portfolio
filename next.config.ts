f4078c6601ecd1a040fbd3da540827d099f768ee
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-30 17:08:52 -0300

    feat: next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/portfolio', // nome do repositório no GitHub
+  assetPrefix: '/portfolio/',
+  images: {
+    unoptimized: true, // Necessário pois o componente de imagem otimizada do Next.js precisa de servidor
+  }
 };
 
 export default nextConfig;
