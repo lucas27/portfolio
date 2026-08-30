bf5cd98b76a576c737356654a4e8dc48a4cd279e
lucas27 <lucasbrasileiro27@gmail.com>
2026-08-30 17:34:44 -0300

    remove: old lines

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/portfolio', // nome do repositório no GitHub
-  assetPrefix: '/portfolio/',
-  images: {
-    unoptimized: true, // Necessário pois o componente de imagem otimizada do Next.js precisa de servidor
-  }
 };
 
 export default nextConfig;
