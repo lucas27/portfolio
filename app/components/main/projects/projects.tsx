6f337b32ef9ddfbbe505d9c293408a0053310b35
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-16 17:36:40 -0300

    feat: split components

--- app/components/main/projects/projects.tsx
+++ app/components/main/projects/projects.tsx
@@ -1,80 +1,14 @@
-import arrow from '@/public/icons8-arrow-right-24.png';
-import arrowWhite from '@/public/icons8-arrow-right-24-white.png';
-import cartAnimationIcon from '@/public/icons8-cart.gif';
-import avatarIcon from '@/public/icons8-profile-avatar-50.png';
-import soundAnimationIcon from '@/public/icons8-sound.gif';
-import fileIcon from '@/public/icons8-file.gif';
-import cardIcon from '@/public/icons8-card-exchange.gif';
-
 import Service from "@/app/service/Service";
-import description from './description.json';
 
 import { ThemeContext } from "@/app/utils/context";
-import { Suspense, use, useContext, useEffect, useState } from "react";
+import { Suspense, useContext, useEffect, useState } from "react";
+import { ProjectRepository } from "./components/projectRepository";
+import { CacheSession } from "./components/cache";
 
-type Project = { 
+export type Project = { 
     name: string; 
     clone_url: string; 
 };
-
-const cacheSession = async (request: Promise<Project[]>):Promise<Project[]> => {
-    const cache = sessionStorage.getItem("projectsCache");
-
-    if(cache) {
-        return JSON.parse(cache);
-    }
-
-    const data = await request;
-    sessionStorage.setItem("projectsCache", JSON.stringify(data));
-
-    return data;
-}
-
-const ProjectRepository = ({theme, resp }: {theme : string | undefined, resp: Project[]}) => {
-    const [hover, setHover] = useState<number | null>(null);
-
-    const icons = [
-        fileIcon.src,
-        cartAnimationIcon.src,
-        cardIcon.src,
-        avatarIcon.src,
-        soundAnimationIcon.src
-    ]
-
-    return resp.map((projects, index) => (
-        <li 
-        key={index}
-        className="flex gap-4 border-1 border-transparent rounded-2xl shadow-xl p-5 h-45 justify-center" 
-        style={{backgroundColor: theme?.includes("black") ? "white" : "#171B25"}}
-        >
-            <img className="border-1 rounded-xl bg-white border-transparent shadow-[0_0_15px_rgba(0,0,0,0.3)] h-15 w-15 p-3" src={icons[index]} />
-            <div className="flex relative flex-col w-120 ">
-                <h1 className="font-[Inter] text-xl font-bold"
-                style={{color: theme}}
-                >{projects.name.charAt(0).toUpperCase() + projects.name.substring(1)}</h1>
-                <p className="font-[Roboto] text-lg">{description[index]}</p>
-                <a href={projects.clone_url} target="_blank"
-                onMouseEnter={() => setHover(index)}
-                onMouseLeave={() => setHover(null)}
-                style={{
-                    backgroundColor: hover === index ? "#FF3B3B" : "transparent", 
-                    color: hover === index ? "white" : 
-                    theme?.includes("black") ? "#FF3B3B" : theme, 
-                    borderColor: theme?.includes("black") ? "#FF3B3B" : hover !== index ? "gray" : "transparent"
-                }}
-                className="flex absolute bottom-[-5px] border-1 font-[Roboto] text-lg w-40 h-12 justify-center items-center gap-3 rounded-xl select-none cursor-pointer">
-                    {"Ver projetos"}
-                    <img src={
-                        theme?.includes("black") && hover !== index ?
-                        arrow.src
-                        :
-                        arrowWhite.src
-                        } /> 
-                </a>
-            </div>
-        </li>
-    ));
-}
 
 function Projects() {
     const [projects, setProjects] = useState<Project[]>([]); 
@@ -82,9 +16,9 @@ function Projects() {
     const textColor = color?.themeMode; 
     
     useEffect(() => {
-       const load = async () => {
+        const load = async () => {
             const service = new Service().getRequest() as unknown as Promise<Project[]>;
-            const resp = await cacheSession(service);
+            const resp = await CacheSession(service);
             setProjects(resp);
         }
         load()
@@ -96,7 +30,7 @@ function Projects() {
             <h3 className="flex text-4xl font-[Inter] after:absolute after:border-b-2 after:border-[#FF3B3B] after:w-19 after:h-12"
             style={{color: textColor}}
             >Projetos</h3>
-            <ul className="flex flex-row flex-wrap gap-3">
+            <ul className="flex flex-row flex-wrap gap-3 relative">
                 <Suspense >
                     <ProjectRepository theme={textColor} resp={projects}/>
                 </Suspense>
